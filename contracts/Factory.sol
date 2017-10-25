ce9534deb043764578d8a589d4fbf9d2ad7127e0
Delphi Systems <contact@delphi.systems>
2017-10-24 22:54:18 -0500

    Version pragma update

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.15;
+pragma solidity ^0.4.15;
 
 contract Factory {
     mapping(address => bool) public isInstantiation;
--- contracts/PythianOracle.sol
+++ contracts/PythianOracle.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.15;
+pragma solidity ^0.4.15;
 
 contract PythianOracle {
     uint constant public MAX_ORACLE_COUNT = 50;
